b2ea4804721ec86cd97d48d17e5ad92ae75aeb94
kabeerx9 <kabeer786joshi@gmail.com>
2025-12-30 20:16:53 +0530

    Setup github action for server ci/cd -4

--- server/src/index.js
+++ server/src/index.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 // Health check root - moved to top to ensure availability even if config is incomplete
 app.get("/", (req, res) => {
-  res.send({ status: "ok", message: "Kabeer Testing CI/CD Pipeline" });
+  res.send({ status: "ok", message: "Kabeer Testing CI/CD Pipeline in new repo." });
 });
 
 app.use(helmet());
